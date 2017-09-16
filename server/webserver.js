ae94268bf38109565fff634fb7a9a45075c0a616
gtyhughes <graysonhughes@gmail.com>
2017-09-16 09:01:02 -0700

    gitignore changes

--- server/webserver.js
+++ server/webserver.js
@@ -4,6 +4,23 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 
 const webserver = express();
+
+webserver.get('/', function(req,res){
+
+    const {host, port, username: user , password, db: database} = credentials;
+    const newCredentials = {host, port, user, password, database};
+    console.log(newCredentials);
+    const connection = mysql.createConnection(newCredentials);
+    connection.connect(function(){
+        console.log('connected to database');
+        connection.query("SELECT * FROM users", (err, rows, fields) => {
+            //res.end(JSON.stringify(rows));
+            res.end(rows);
+            //console.log(rows);
+        });
+    });
+    //res.end('get off my lawn');
+});
 webserver.post('/users', function(req,res){
     console.log("post variables", req.body);
     const {host, port, username: user , password, db: database} = credentials;
@@ -27,22 +44,6 @@ webserver.post('/users', function(req,res){
         //incomplete, didn't capture all of Dan's coding
     });
 })
-webserver.get('/', function(req,res){
-
-    const {host, port, username: user , password, db: database} = credentials;
-    const newCredentials = {host, port, user, password, database};
-    console.log(newCredentials);
-    const connection = mysql.createConnection(newCredentials);
-    connection.connect(function(){
-        console.log('connected to database');
-        connection.query("SELECT * FROM users", (err, rows, fields) => {
-            //res.end(JSON.stringify(rows));
-            res.end(rows);
-            //console.log(rows);
-        });
-    });
-    //res.end('get off my lawn');
-});
 webserver.listen(3000, function(){
     console.log('Example listening on port 3000');
 });
