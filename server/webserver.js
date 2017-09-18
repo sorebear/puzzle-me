8e3bbbde679cfed7763f9468558c3aff0292a329
gtyhughes <graysonhughes@gmail.com>
2017-09-17 20:57:45 -0700

    enabled client-side retrieval of recent puzzles from database - GH

--- server/webserver.js
+++ server/webserver.js
@@ -2,10 +2,44 @@ const express = require('express');
 var credentials = require('./mysqlCredentials');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+
 const webserver = express();
 
-
+webserver.use(bodyParser.urlencoded({ extended: false }))
+webserver.use(bodyParser.json());
 webserver.use(express.static('public'));
+
+const pool = mysql.createPool(credentials);
+pool.getConnection(function(err, conn){
+   if(err) console.log("Error connecting to MySQL database");
+});
+
+// webserver.get('/puzzles', function(req, res){
+//     console.log("req.query.retrieve is: ", req.query.retrieve);
+//     if(req.query.retrieve){
+//         switch(req.query.retrieve) {
+//             case 'recent10':
+//                 getMostRecent10Puzzles(res);//grab 10 most recent puzzles from database
+//                 break;
+//             default:
+//                 console.log("unknown query value for puzzles key");
+//         }
+//     }
+//     else{
+//         console.log("Query key puzzles is not present");
+//     }
+// });
+//
+// function getMostRecent10Puzzles(res){
+//     var query = "SELECT p.puzzle_name, u.username AS creator, p.type, p.size, p.url_ext, p.likes, p.dislikes, p.date_created " +
+//                 "FROM `puzzles` AS `p` " +
+//                 "JOIN `users` AS `u`" +
+//                 "ON p.creator_id = u.u_id";
+//     pool.query(query, (err, rows, fields) => {
+//         if(err) console.log(err);
+//         else res.end(JSON.stringify({success: true, data: rows}));
+//     });
+// }
 
 // webserver.get('/',function(req,res){
 //     console.log('sending data of file',__dirname);
@@ -59,6 +93,6 @@ webserver.use(express.static('public'));
 //     });
 // })
 webserver.listen(3000, function(){
-    console.log('Example listening on port 3000');
+    console.log('Webserver listening on port 3000');
 });
 
