be5666099c7fb75bd4589e16edf6a25de78c9733
Daniel Paschal <daniel.paschal@learningfuze.com>
2017-10-06 06:55:11 -0700

    fixed preflight issue with headers

--- server/webserver.js
+++ server/webserver.js
@@ -30,7 +30,7 @@ webserver.use(cookieSession({
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'access-control-allow-origin, Content-Type, Authorization, Content-Length, X-Requested-With');
   res.header('Access-Control-Allow-Credentials',true);
   next();
 };
